b0a5f7fd2887b4551905904d9693aae8e7a776c6
Kristofer Fonagy <kristofer@fonagy.com>
2021-10-09 16:54:36 -0400

    * History.js: Create ../components/Requests/History.js > displays   all requests for current user
    * ReqDropdown.js: Create ../components/Requests/ReqDropdown.js >
      dropdown menu for controlling requests view filter
    
    * TimeOff.Request.DataAccess.EntityFrameworkCore.targets: Create
      ...EntityFrameworkCore.targets

---
+++ TimeOff.Request/ClientApp/src/components/Requests/History.js
@@ -0,0 +1,84 @@
+﻿import React, { Component } from 'react';
+import { Table, Button, Row } from 'reactstrap';
+import Moment from 'moment';
+import ReqDropdown from './ReqDropdown';
+import { Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+export class History extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            requests: [],
+            loading: true,
+            testId: 2
+        };
+
+        
+    }
+
+    componentDidMount() {
+        this.populateRequestData();
+    }
+
+    static renderReqsTable(requests) {
+        return (
+            <Table dark hover>
+                <thead>
+                    <tr>
+                        <th>Created Date</th>
+                        <th>Approved Date</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Days</th>
+                        <th>Disabled</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {requests.map(req =>
+                        <tr key={req.id}>
+                            <td>{ Moment(req.createdDate).format('LL') }</td>
+                            <td>{ Moment(req.approvedDate).format('LL') }</td>
+                            <td>{ Moment(req.startDate).format('LL') }</td>
+                            <td>{ Moment(req.endDate).format('LL') }</td>
+                            <td>{ req.numberOfDays }</td>
+                            <td>
+                                <Button type="checkbox" value={req.disabled}></Button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : History.renderReqsTable(this.state.requests);
+
+        return (
+            <div>
+                <Row>
+                    <Col>
+                        <h1 id="tabelLabel" >Request History</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ReqDropdown />
+                    </Col>
+                </Row>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateRequestData() {
+        const response = await fetch('api/request/CreatedBy/' + this.state.testId);
+        const data = await response.json();
+        console.log(data)
+        if (data.length > 0) {
+            this.setState({ requests: data, loading: false });
+        }
+    }
+}
---
+++ TimeOff.Request/ClientApp/src/components/Requests/ReqDropdown.js
@@ -0,0 +1,27 @@
+﻿import React, { useState } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const ReqDropdown = (props) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+    const [lastClicked, setLastClicked] = useState(null);
+
+    return (
+        <div >
+            <Dropdown group isOpen={dropdownOpen} size="sm" toggle={toggle}>
+                <DropdownToggle caret>
+                    {lastClicked || 'All'}
+                </DropdownToggle>
+                <DropdownMenu container="body">
+                    <DropdownItem onClick={() => setLastClicked('All')}>All Requests</DropdownItem>
+                    <DropdownItem onClick={() => setLastClicked('Timeframe')}>By Timeframe</DropdownItem>
+                    <DropdownItem onClick={() => setLastClicked('Status')}>By Approval</DropdownItem>
+                    <DropdownItem onClick={() => setLastClicked('Open')}>Open Requests</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+    );
+}
+
+export default ReqDropdown;
