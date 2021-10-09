e79259e8ab62afe18e3eda276fd8457c81316904
Kristofer Fonagy <kristofer@fonagy.com>
2021-10-09 19:31:57 -0400

    * ReqDropdown.js: Move dropdown to History.js, delete ReqDropdown
    * RequestTable.js: Create RequestTable.js, move requests table from
      History.js to RequestTable.js
    
    * History.js: Add RequestTable component from RequestTable.js, add
      request dropdown menu, add initial filter logic

--- TimeOff.Request/ClientApp/src/components/Requests/History.js
+++ TimeOff.Request/ClientApp/src/components/Requests/History.js
@@ -1,7 +1,7 @@
 ﻿import React, { Component } from 'react';
 import { Table, Button, Row } from 'reactstrap';
 import Moment from 'moment';
-import ReqDropdown from './ReqDropdown';
+import RequestTable from './RequestTable';
 import { Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export class History extends Component {
@@ -9,49 +9,66 @@ export class History extends Component {
         super(props);
         this.state = {
             requests: [],
+            allReq: [],
             loading: true,
+            lastClicked: 'All',
+            open: false,
             testId: 2
         };
 
-        
+        this.curDate = new Date();
     }
 
     componentDidMount() {
         this.populateRequestData();
     }
 
+    toggle = () => {
+        this.setState({
+            open: !open
+        })
+    }
+
+    setLastClicked = (value) => {
+        if (value === 'All') {
+            this.setState({
+                requests: this.state.allReq,
+                lastClicked: value
+            })
+        }
+        else if (value === 'Timeframe') {
+            let sorted = this.state.allReq.sort((a, b) => b.startDate > a.startDate ? 1 : -1);
+            this.setState({
+                requests: sorted,
+                lastClicked: value
+            })
+        }
+        else if (value === 'Approval') {
+            // need to identify how we want to sort this
+
+        }
+        else if (value === 'Status') {
+            let sorted = this.state.allReq.filter(req => req.endDate < this.curDate);
+            this.setState({
+                requests: sorted,
+                lastClicked: value
+            })
+        }
+    } 
+
     static renderReqsTable(requests) {
         return (
-            <Table dark hover>
-                <thead>
-                    <tr>
-                        <th>Created Date</th>
-                        <th>Approved Date</th>
-                        <th>Start Date</th>
-                        <th>End Date</th>
-                        <th>Days</th>
-                        <th>Disabled</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {requests.map(req =>
-                        <tr key={req.id}>
-                            <td>{ Moment(req.createdDate).format('LL') }</td>
-                            <td>{ Moment(req.approvedDate).format('LL') }</td>
-                            <td>{ Moment(req.startDate).format('LL') }</td>
-                            <td>{ Moment(req.endDate).format('LL') }</td>
-                            <td>{ req.numberOfDays }</td>
-                            <td>
-                                <Button type="checkbox" value={req.disabled}></Button>
-                            </td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
+            <RequestTable requests={requests} />
         );
     }
 
     render() {
+        const dropdown = [
+            { id: 1, name: 'All Requests', value: 'All' },
+            { id: 2, name: 'By Timeframe', value: 'Timeframe' },
+            { id: 3, name: 'By Approval', value: 'Approval' },
+            { id: 4, name: 'Completion Status', value: 'Status' }
+        ]
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
             : History.renderReqsTable(this.state.requests);
@@ -65,7 +82,22 @@ export class History extends Component {
                 </Row>
                 <Row>
                     <Col>
-                        <ReqDropdown />
+                        <div>
+                            <Dropdown group isOpen={this.state.open} size="sm" toggle={this.toggle}>
+                                <DropdownToggle caret>
+                                    {this.state.lastClicked}
+                                </DropdownToggle>
+                                <DropdownMenu container="body">
+                                    {dropdown.map((id, name, value) =>
+                                        <DropdownItem
+                                            key={id}
+                                            value={value}
+                                            onClick={this.setLastClicked.bind(this, value)}
+                                        >{name}</DropdownItem>
+                                    )}
+                                </DropdownMenu>
+                            </Dropdown>
+                        </div>
                     </Col>
                 </Row>
                 {contents}
@@ -78,7 +110,11 @@ export class History extends Component {
         const data = await response.json();
         console.log(data)
         if (data.length > 0) {
-            this.setState({ requests: data, loading: false });
+            this.setState({
+                requests: data,
+                allReq: data,
+                loading: false
+            });
         }
     }
 }
---
+++ TimeOff.Request/ClientApp/src/components/Requests/RequestTable.js
@@ -0,0 +1,46 @@
+﻿import React, { Component } from 'react';
+import { Table, Button, Row } from 'reactstrap';
+import Moment from 'moment';
+import ReqDropdown from './ReqDropdown';
+
+export class RequestTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            requests: props.requests
+        }
+    }
+
+    render() {
+        return (
+            <Table dark hover>
+                <thead>
+                    <tr>
+                        <th>Created Date</th>
+                        <th>Approved Date</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Days</th>
+                        <th>Disabled</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.requests.map(req =>
+                        <tr>
+                            <td>{Moment(req.createdDate).format('LL')}</td>
+                            <td>{Moment(req.approvedDate).format('LL')}</td>
+                            <td>{Moment(req.startDate).format('LL')}</td>
+                            <td>{Moment(req.endDate).format('LL')}</td>
+                            <td>{req.numberOfDays}</td>
+                            <td>
+                                <Button type="checkbox" value={req.disabled}></Button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+        )
+    }
+}
+
+export default RequestTable;
