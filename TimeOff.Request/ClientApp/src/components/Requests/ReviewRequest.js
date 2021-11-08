f46123cab08151fd9c460dbca1e385dadcb02946
Kristofer Fonagy <kristofer@fonagy.com>
2021-11-07 22:35:37 -0500

    * TimeOffContext.db: Testing
    * ReviewRequest.js: Update ReviewRequest to add back days off to User
      numberOfDaysOff if denied, set up view to show approved active
      requests, fetch request createdById user info

--- TimeOff.Request/ClientApp/src/components/Requests/ReviewRequest.js
+++ TimeOff.Request/ClientApp/src/components/Requests/ReviewRequest.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 import Moment from 'moment';
 
 const updateRequest = async values => {
-    console.log(values);
     const url = '/api/Request/' + values.id;
     const resp = await fetch(url, {
         method: 'PUT',
@@ -30,6 +29,20 @@ const updateRequest = async values => {
     return resp.json();
 }
 
+const updateUser = async values => {
+    const url = '/api/User/' + values.id;
+    const resp = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            ...values.requester
+        })
+    })
+    return resp.json();
+}
+
 export class ReviewRequest extends Component {
     constructor(props) {
         super(props);
@@ -43,7 +56,10 @@ export class ReviewRequest extends Component {
             archived: false,
             approved: null,
             canceled: false,
-            show: false
+            show: false,
+            active: null,
+            sorted: [],
+            requester: null
         }
 
         this.onApprove = this.onApprove.bind(this)
@@ -65,7 +81,8 @@ export class ReviewRequest extends Component {
         let canceled = this.state.request.canceled
         let archived = this.state.request.archive
         let disabled = this.state.request.disabled
-
+        let sorted = this.state.active.filter(r => r.createdById !== this.state.request.createdById)
+        
         if (this.state.request.approvedById === null) {
             status = 'Awaiting Approval'
         } else if (this.state.request.canceled === true) {
@@ -80,7 +97,7 @@ export class ReviewRequest extends Component {
             show = true
         }
 
-        this.setState({ status, show, approved, canceled, archived, disabled })
+        this.setState({ status, show, approved, canceled, archived, disabled, sorted })
     }
 
     onApprove() {
@@ -91,9 +108,14 @@ export class ReviewRequest extends Component {
     }
 
     onDeny() {
+        const days = this.state.request.numberOfDays
+        const daysOff = this.state.requester.numberOfDaysOff
         this.setState({
             canceled: true,
-            status: 'Denied'
+            status: 'Denied',
+            requester: {
+                numberOfDaysOff: days + daysOff
+            }
         })
     }
 
@@ -111,6 +133,9 @@ export class ReviewRequest extends Component {
 
     async onSubmit(event) {
         event.preventDefault();
+        if (this.state.canceled) {
+            await updateUser(this.state)
+        }
         const values = {
             id: this.state.request.id,
             createdBy: this.state.request.createdById,
@@ -225,8 +250,42 @@ export class ReviewRequest extends Component {
                         }
                     </FormGroup>
                 </Form>
+                
             </div>
         )
+    }
+
+    returnActiveRequest(state) {
+        const approved = state.sorted.filter(r => r.approvedById === state.userId)
+        if (approved.length > 0) {
+            return (
+                <div>
+                    <h6>Active Approved Requests</h6>
+                    <Table dark>
+                        <thead>
+                            <tr>
+                                <td>User</td>
+                                <td>Start Date</td>
+                                <td>End Date</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {approved.map(r =>
+                                <tr key={r.id}>
+                                    <td>{r.createdBy.name}</td>
+                                    <td>{Moment(r.startDate).format('LL')}</td>
+                                    <td>{Moment(r.endDate).format('LL')}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
+                </div>
+            )
+        } else {
+            return (
+                <div><p><em>No Active Approved Requests</em></p></div>
+            )
+        }
     }
 
     render() {
@@ -241,6 +300,12 @@ export class ReviewRequest extends Component {
                         : this.renderReviewRequest(this.state)
                     }
                 </Row>
+                <Row key='2' style={{marginTop: 4}}>
+                    <div>
+                        {!this.state.loading && this.returnActiveRequest(this.state)
+                        }
+                    </div>
+                </Row>
             </div>
         )
     }
@@ -248,19 +313,26 @@ export class ReviewRequest extends Component {
     async populateRequestData() {
         const id = this.props.match.params.id;
         const userId = Number.parseInt(localStorage.getItem('user'), 10);
+        const curDate = Moment(new Date()).format('LL')
         const response1 = await fetch('api/User/' + userId)
         const data1 = await response1.json();
         const isAdmin = data1.roles === 'Administrator'
         const response2 = await fetch('api/Request/' + id);
         const data2 = await response2.json();
-        console.log(data2)
+        const response3 = await fetch('api/Request/Active');
+        const data3 = await response3.json();
+        const response4 = await fetch('api/User/' + data2.createdById)
+        const data4 = await response4.json();
+        
         if (data2 !== null) {
             this.setState({
                 request: data2,
                 loading: false,
                 admin: isAdmin,
                 userId: userId,
-                date: data2.approvedDate
+                date: data2.approvedDate,
+                active: data3.filter(r => (r.createdBy.supervisorId === userId) && (Moment(r.endDate).format('LL') < curDate && !r.archived)),
+                requester: data4
             })
         }
     }
